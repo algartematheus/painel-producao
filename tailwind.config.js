e2dbfc8346afd5550e1d17393f281c4677f65a08
algartematheus <devilbloods940@gmail.com>
2025-09-25 13:54:38 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // Esta linha é a mais importante.
+  // Diz ao Tailwind para procurar por classes em todos estes arquivos.
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
+  // Esta linha ativa o modo escuro manual através da classe 'dark'
+  darkMode: 'class',
   theme: {
     extend: {},
   },
