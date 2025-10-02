43b51242afb2875eeffb1c2e284774a47485b449
algartematheus <devilbloods940@gmail.com>
2025-10-02 11:20:59 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,9 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// Se você quer que seu aplicativo funcione offline e carregue mais rápido,
-// você pode mudar unregister() para register() abaixo. Note que isso vem com algumas armadilhas.
-// Saiba mais sobre service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
-
