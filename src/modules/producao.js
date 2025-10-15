f0768e5131ddc2674d377510c93305807a6e753a
algartematheus <devilbloods940@gmail.com>
2025-10-15 14:21:31 -0300

    Add configurable blank PDF export for operational sequences

---
+++ src/modules/producao.js
@@ -0,0 +1,18 @@
+export {
+  getOrderedActiveLots,
+  getLotRemainingPieces,
+  splitGoalSegments,
+  joinGoalSegments,
+  sumGoalDisplay,
+  formatDefaultLotDisplayName,
+  createProductionRowFromDetail,
+  computeDefaultPredictionsForEdit,
+  buildRowsFromPredictions,
+  areProductionRowsEqual,
+  computeMetaFromStandardTime,
+  computeEfficiencyPercentage,
+  buildProductNames,
+  buildNumericSegments,
+  formatSegmentedNumbers,
+  formatGoalBlockDisplay
+} from './shared';
