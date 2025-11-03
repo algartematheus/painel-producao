6700859f274ff2e113f0ceef220d182999a17b34
algartematheus <devilbloods940@gmail.com>
2025-11-03 18:25:42 -0300

    Point stock importer worker to legacy mjs entry

--- src/modules/stockImporter.js
+++ src/modules/stockImporter.js
@@ -1,6 +1,6 @@
 import { read, utils } from 'xlsx';
-import { GlobalWorkerOptions, getDocument as getDocumentFromPdfjs } from 'pdfjs-dist';
-import pdfWorkerSrc from 'pdfjs-dist/legacy/build/pdf.worker.js';
+import { GlobalWorkerOptions, getDocument as getDocumentFromPdfjs } from 'pdfjs-dist/legacy/build/pdf';
+import pdfWorkerSrc from 'pdfjs-dist/legacy/build/pdf.worker.mjs';
 
 const REF_REGEX = /^([A-Z0-9]{2,}\.[\w-]+)/i;
 const GRADE_LABEL_REGEX = /grade/i;
